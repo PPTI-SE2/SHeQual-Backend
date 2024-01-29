5d419cd58f6d997fbefd4d2c44ded4196d7e7081
Yogesvara <77664705+J-Orlant@users.noreply.github.com>
2024-01-29 10:16:37 +0700

    Create Migration Database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
+            $table->integer('age');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string("img_profile");
+            $table->integer("poin");
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2024_01_29_011026_create_game_histories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('game_histories', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("users_id");
+            $table->bigInteger("question_games_id");
+            $table->integer("correct");
+            $table->integer("status_game");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('game_histories');
+    }
+};
