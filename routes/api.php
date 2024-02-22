ee37f2835f94b0d75a342a0af42b65ef5c6b98ec
Michello <michellorayhan14@gmail.com>
2024-02-22 08:40:22 +0700

    test

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\API\UserController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+//test
 
 Route::post('/register',[AuthController::class, 'register']);
 Route::post('/login',[AuthController::class, 'login']);
